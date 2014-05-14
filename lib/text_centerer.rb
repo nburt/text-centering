0bacb4687e634d3d52da5628129bdbeddc45b75e
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-14 10:22:33 -0600

    refactoring

--- lib/text_centerer.rb
+++ lib/text_centerer.rb
@@ -6,27 +6,15 @@ class TextCenterer
 
   def center
     lines_of_text = @text.split("\n")
-    stripped_lines_of_text = []
-    lines_of_text.each do |line|
-      stripped_line = line.strip
-      stripped_lines_of_text << stripped_line
+    stripped_lines_of_text = lines_of_text.map do |line|
+      line.strip
     end
 
     longest_line = stripped_lines_of_text.sort_by { |line| line.length }.last.length
 
-    centered_text = ""
-    buffer = 0
-    stripped_lines_of_text.each do |line|
-      if line.length < longest_line
-        if (longest_line - line.length).even?
-          buffer = (longest_line - line.length) / 2
-        else
-          buffer = (longest_line - line.length) / 2
-        end
-      end
-      centered_text << "#{' ' * buffer }#{line}\n"
-    end
-    centered_text.chop
+    stripped_lines_of_text.map do |line|
+      line.center(longest_line).rstrip
+    end.join("\n")
   end
 
 end
